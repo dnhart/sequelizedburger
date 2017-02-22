021fcba6e606be6c2bee85a9f2b048fd36374623
FBIMOBILEUNIT\D'Ann <me@dnhart.com>
2017-02-21 21:11:40 -0600

    making sure all changes are pushed

--- seqburger.sql
+++ seqburger.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `burgers_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `burgers_db`;
 -- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
 -- Host: localhost    Database: burgers_db
@@ -24,11 +26,12 @@ DROP TABLE IF EXISTS `burgers`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `burgers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `burger_name` varchar(50) NOT NULL,
-  `devoured` tinyint(1) DEFAULT '0',
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `burger_name` varchar(255) NOT NULL,
+  `devoured` tinyint(1) NOT NULL DEFAULT '0',
+  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,8 +40,7 @@ CREATE TABLE `burgers` (
 
 LOCK TABLES `burgers` WRITE;
 /*!40000 ALTER TABLE `burgers` DISABLE KEYS */;
-
-INSERT INTO `burgers` VALUES (1,'hamburger',1,'2017-02-14 00:01:37','2017-02-14 00:01:37'),(2,'cheeseburger',0,'2017-02-14 00:01:37','2017-02-14 00:01:37'),(3,'turkey burger',0,'2017-02-14 00:01:37','2017-02-14 00:01:37');
+INSERT INTO `burgers` VALUES (1,'hamburger',1,'2017-02-14 00:01:37','2017-02-14 00:01:37'),(2,'cheeseburger',0,'2017-02-14 00:01:37','2017-02-14 00:01:37'),(3,'turkey burger',1,'2017-02-14 00:01:37','2017-02-22 02:31:26'),(4,'hamburger',0,'2017-02-21 17:06:24','2017-02-21 17:06:24'),(5,'aburger',1,'2017-02-22 01:01:17','2017-02-22 02:05:19'),(6,'a new burger',0,'2017-02-22 01:15:07','2017-02-22 01:15:07'),(7,'avocado burger',0,'2017-02-22 01:16:10','2017-02-22 01:16:10'),(8,'bacon cheese burger',0,'2017-02-22 01:18:20','2017-02-22 01:18:20'),(9,'Emily\'s Burger',0,'2017-02-22 02:31:41','2017-02-22 02:31:41');
 /*!40000 ALTER TABLE `burgers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -59,4 +61,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-02-16  9:04:57
+-- Dump completed on 2017-02-21 21:05:56
